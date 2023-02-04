60ad51cdd00a1040115d4bc39eac5731d0a5fbfb
Mighri <mighri.houssem.official@gmail.com>
2023-02-04 18:39:14 +0100

    Spring Boot Micro services: Service Registry Load Balancing

--- employee-sevice/src/main/java/com/tutorial/employeesevice/service/APIClient.java
+++ employee-sevice/src/main/java/com/tutorial/employeesevice/service/APIClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8080", name = "DEPARTMENT-SERVICE")
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface APIClient {
 
     //Department REST API get call
